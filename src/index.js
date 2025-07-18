a364c1aa070ba8fa6ee03c17f9fbca42d4250829
bruhjabi <ammarahmedkhan29@gmail.com>
2025-07-18 11:37:55 -0400

    Basic projects and todo DOM logic done. LocalStorage partially implemented

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import "./styles.css";
 import { Task, Project } from "./todos.js";
 
 
-const projects = new Map()
-const currentProject = "Default"
+console.log(localStorage.projects == undefined)
+const projects = localStorage.projects == undefined ? new Map() : new Map(JSON.parse(localStorage.getItem('projects')))
+let currentProject = "Default"
+
+const container = document.querySelector(".container")
+const sidebar = document.querySelector(".sidebar")
 
 projects.set("Default", new Project("Default"));
 projects.set("Home", new Project("Home"));
@@ -19,23 +23,27 @@ projects.get("Home").addTask(new Task("Fix sink", "Call the plumber", "2025-07-2
 projects.get("Work").addTask(new Task("Prepare report", "Weekly team report", "2025-07-19", "High"));
 projects.get("Work").addTask(new Task("Email client", "Follow up on proposal", "2025-07-18", "Medium"));
 
-const sidebar = document.querySelector(".sidebar")
-
-
 projects.forEach(project => {
     const projectDOM = document.createElement("button")
+    projectDOM.id = project.name
     projectDOM.innerText = project.name
+
+    projectDOM.addEventListener("click", function(e) {
+        currentProject = this.id
+        renderTasks()
+    })
+
     sidebar.appendChild(projectDOM)
 });
 
-console.log(projects)
+function renderTasks() {
+    container.innerHTML = "";
 
-const container = document.querySelector(".container")
+    projects.get(currentProject).tasks.forEach(task => {
+        const taskDOM = document.createElement("div")
+        taskDOM.innerText = task.title + task.description + task.dueDate + task.priority + task.id
+        container.appendChild(taskDOM)
+    });
+}
 
-projects.get(currentProject).tasks.forEach(task => {
-    const taskDOM = document.createElement("div")
-    taskDOM.innerText = task.title + task.description + task.dueDate + task.priority + task.id
-    container.appendChild(taskDOM)
-});
-
-console.log(projects.get(currentProject).tasks)
+localStorage.projects = JSON.stringify([...projects])
